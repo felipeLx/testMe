6a0ed675c90133c8459d67fdfaea3b768b76a12c
felipeLx <felipealvesfinanceiro@gmail.com>
2023-07-19 18:29:32 -0300

    :rocket: preparing for deploy

--- index.py
+++ index.py
@@ -1,10 +1,15 @@
 import streamlit as st
 from mercado_libre_data import MercadoLibreData
 
+# Initialize the session state variable.
+if 'download_clicked' not in st.session_state:
+    st.session_state.download_clicked = False
+
 # User input
 product_name = st.sidebar.text_input(label='Diga el producto que busca', placeholder='El Camino de Santiago')
 user_items_limit = st.sidebar.number_input(label='Informar la cantidad de items', min_value=150)
-st.image('./logo.png', caption='Mercado Libre')    
+st.image('./logo.png', caption='Mercado Libre')
+
 # Create an instance of the MercadoLibreData class.
 ml_data = MercadoLibreData(product_name, user_items_limit)
 
@@ -23,11 +28,18 @@ if len(ml_data.df) != 0:
     data = ml_data.generate_hyper_file()
 
     # Body
-    
     st.write('''
             # Sigue los datos solicitados
             ''', unsafe_allow_html=False)
     st.write(ml_data.df)
 
-    
-    st.sidebar.download_button('Guardar Hyper', data, file_name=product_name+'.hyper')
+    # Add a download button.
+    if st.sidebar.download_button('Guardar Hyper', data, file_name=product_name+'.hyper'):
+        # Set the session state variable to True and rerun the script.
+        st.session_state.download_clicked = True
+        st.experimental_rerun()
+
+    # Check if the download button has been clicked.
+    if st.session_state.download_clicked:
+        # Display an alert.
+        st.success(f'Archivo {product_name}.hyper descargado con éxito! Verifique su carpeta de descargas.')
